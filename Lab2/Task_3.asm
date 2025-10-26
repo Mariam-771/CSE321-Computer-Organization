0e07fce91220d9d9d7dd4bd1a1c84cd7ecb0fb21
Mariam Adel <mariamadel4910@gmail.com>
2025-10-26 22:46:22 +0300

    Refactor switch-case comparisons to use $t1
    Refactor switch-case structure to use a variable for comparison.

--- Lab2/Task_3.asm
+++ Lab2/Task_3.asm
@@ -19,31 +19,29 @@ move $t0, $v0      # choice
 switch:
 
 li $t1, 1
-beq $t0, 1, case1
+beq $t0, $t1, case1
 
 li $t1, 2
-beq $t0, 2, case2
+beq $t0, $t1, case2
 
 j default
 
 case1: 
-       li $v0, 4
+
        la $a0, case1_msg
-       syscall
        j endcase
 case2:
-       li $v0, 4
+
        la $a0, case2_msg
-       syscall
         j endcase
 default:
-       li $v0, 4
+
        la $a0, other
-       syscall
         j endcase
 
 endcase:
-
+        li $v0, 4
+        syscall
 exit:
 li $v0, 10
 syscall
